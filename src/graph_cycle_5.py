ddb2f8cbfce04ee2770b9cc12a54b4d8db8b2c5d
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-04-09 10:06:43 +0300

    Update graph_cycle_5.py
    Change from BFS to DFS

--- src/graph_cycle_5.py
+++ src/graph_cycle_5.py
@@ -1,29 +1,22 @@
 from collections import defaultdict, deque
 
-
 def is_cyclic(graph):
     visited = set()
     for node in graph:
         if node not in visited:
-            if bfs(node, graph, visited):
+            if dfs(node, graph, visited, None):
                 return True
     return False
 
-
-def bfs(start, graph, visited):
-    queue = deque([(start, None)])
-
-    while queue:
-        node, parent = queue.popleft()
-        visited.add(node)
-
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                queue.append((neighbor, node))
-            elif neighbor != parent:
+def dfs(start, graph, visited, parent):
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            if dfs(neighbor, graph, visited, start):
                 return True
+        elif neighbor != parent:
+            return True
     return False
-
 
 def read_graph_from_file(filename):
     graph = defaultdict(list)
@@ -34,7 +27,6 @@ def read_graph_from_file(filename):
             neighbors = [int(x) for x in data[1:]]
             graph[node] = neighbors
     return graph
-
 
 def main():
     graph = read_graph_from_file("input.txt")
