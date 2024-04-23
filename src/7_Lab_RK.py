93aad6bd3109cd0bbbd09ff59cbe117642ae769c
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-04-23 11:39:45 +0300

    Update 7_Lab_RK.py

--- src/7_Lab_RK.py
+++ src/7_Lab_RK.py
@@ -8,20 +8,17 @@ def rabin_karp_search(haystack, needle):
     base = 26
     modulus = 10 ** 9 + 7
 
-
     needle_hash = 0
     window_hash = 0
     for i in range(needle_len):
         needle_hash = (needle_hash * base + ord(needle[i])) % modulus
         window_hash = (window_hash * base + ord(haystack[i])) % modulus
 
-
     base_l_minus_1 = pow(base, needle_len - 1, modulus)
 
     for i in range(haystack_len - needle_len + 1):
         if window_hash == needle_hash:
-
-            if haystack[i:i + needle_len] == needle:
+            if haystack[i:i + needle_len] == needle:  # Додаткова перевірка рядків
                 result.append(i)
         if i < haystack_len - needle_len:
             window_hash = (window_hash - ord(haystack[i]) * base_l_minus_1) % modulus
