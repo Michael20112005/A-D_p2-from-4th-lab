9f0bae00c3e111409d37cb74144e95095fb3dfdd
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-09 22:40:33 +0300

    Update heap_based_priority_queue.py
    Removed print and corrected grammar for pep-8

--- src/heap_based_priority_queue.py
+++ src/heap_based_priority_queue.py
@@ -1,16 +1,19 @@
-class Task:
-    def __init__(self, description, priority):
-        self.description = description
+class Node:
+    def __init__(self, value, priority):
+        self.value = value
         self.priority = priority
+
 
 class PriorityTaskQueue:
     def __init__(self):
         self.task_heap = []
 
-    def add_task(self, description, priority):
-        task = Task(description, priority)
+    def add_task(self, task):
         self.task_heap.append(task)
         self._reorder_up(len(self.task_heap) - 1)
+
+    def add_new_task(self, value, priority):
+        self.add_task(Node(value, priority))
 
     def get_highest_priority_task(self):
         if not self.task_heap:
@@ -21,40 +24,34 @@ class PriorityTaskQueue:
         self.task_heap.pop()
         self._reorder_down(0)
 
-        return highest_priority_task.description
+        return highest_priority_task.value
 
     def peek_highest_priority_task(self):
-        if self.task_heap:
-            return self.task_heap[0].description
-        return None
+        return self.task_heap[0].value if self.task_heap else None
 
     def _reorder_up(self, index):
         parent_index = (index - 1) // 2
-        if parent_index >= 0 and self.task_heap[index].priority > self.task_heap[parent_index].priority:
+        while (index > 0) and (self.task_heap[index].priority > self.task_heap[parent_index].priority):
             self.task_heap[index], self.task_heap[parent_index] = self.task_heap[parent_index], self.task_heap[index]
-            self._reorder_up(parent_index)
+            index = parent_index
+            parent_index = (index - 1) // 2
 
     def _reorder_down(self, index):
-        left_child_index = 2 * index + 1
-        right_child_index = 2 * index + 2
-        largest = index
+        while True:
+            left_child_index = 2 * index + 1
+            right_child_index = 2 * index + 2
+            largest = index
 
-        if left_child_index < len(self.task_heap) and self.task_heap[left_child_index].priority > self.task_heap[index].priority:
-            largest = left_child_index
-        if right_child_index < len(self.task_heap) and self.task_heap[right_child_index].priority > self.task_heap[largest].priority:
-            largest = right_child_index
+            if left_child_index < len(self.task_heap) and \
+                    self.task_heap[left_child_index].priority > self.task_heap[largest].priority:
+                largest = left_child_index
 
-        if largest != index:
-            self.task_heap[index], self.task_heap[largest] = self.task_heap[largest], self.task_heap[index]
-            self._reorder_down(largest)
+            if right_child_index < len(self.task_heap) and \
+                    self.task_heap[right_child_index].priority > self.task_heap[largest].priority:
+                largest = right_child_index
 
-# Usage example
-task_queue = PriorityTaskQueue()
-task_queue.add_task('to tear out a mannequin_s caddy', 56)
-task_queue.add_task('sink a toy boat', 666)
-task_queue.add_task('throw an apple', 4)
-
-
-print(task_queue.get_highest_priority_task())
-print(task_queue.get_highest_priority_task())
-print(task_queue.get_highest_priority_task())
+            if largest != index:
+                self.task_heap[index], self.task_heap[largest] = self.task_heap[largest], self.task_heap[index]
+                index = largest
+            else:
+                break
