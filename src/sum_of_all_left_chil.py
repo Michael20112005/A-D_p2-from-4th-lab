2883b7ec8c76b0c4052743955a8ab3971b7fa0d1
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-20 23:14:25 +0300

    3_Reduced + no print.py

--- src/sum_of_all_left_chil.py
+++ src/sum_of_all_left_chil.py
@@ -4,20 +4,13 @@ class BinaryTree:
         self.left = None
         self.right = None
 
+
 def branchSums(root):
-    if root is None:
-        return 0
+    def helper(node, is_left):
+        if not node:
+            return 0
+        if not node.left and not node.right and is_left:
+            return node.value
+        return helper(node.left, True) + helper(node.right, False)
 
-    if root.left is not None and root.left.left is None and root.left.right is None:
-        return root.left.value + branchSums(root.right)
-    else:
-        return branchSums(root.left) + branchSums(root.right)
-
-
-root = BinaryTree(3)
-root.left = BinaryTree(9)
-root.right = BinaryTree(20)
-root.right.left = BinaryTree(15)
-root.right.right = BinaryTree(7)
-
-print(branchSums(root))
+    return helper(root, False)
