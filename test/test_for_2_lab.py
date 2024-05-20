d262c1f2f1861f43768d83909c26a1bea51b24f2
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-20 19:57:12 +0300

    tests_2Lab

---
+++ test/test_for_2_lab.py
@@ -0,0 +1,24 @@
+import unittest
+from hashing import find_3_numbers_sum
+
+
+class TestFindThreeNumbers(unittest.TestCase):
+
+    def test_find_numbers_with_sum_good_case(self):
+        array = [1, 2, 3, 6]
+        p = 6
+        self.assertTrue(find_3_numbers_sum(array, p))
+
+    def test_find_three_numbers_LoL_case(self):
+        array = [2, 100, 60, 1]
+        p = 41
+        self.assertFalse(find_3_numbers_sum(array, p))
+
+    def test_find_three_numbers_nonexistent_case(self):
+        array = [2, 3]
+        p = 7
+        self.assertFalse(find_3_numbers_sum(array, p))
+
+
+if __name__ == '__main__':
+    unittest.main()
