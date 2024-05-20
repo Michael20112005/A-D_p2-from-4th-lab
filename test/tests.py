6da234811f85219026cb33ad67f6786b34e575db
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-20 20:25:00 +0300

    test_3Lab

---
+++ test/tests.py
@@ -0,0 +1,28 @@
+import unittest
+from binary_tree import branchSums, BinaryTree
+
+
+class TestBranchSums(unittest.TestCase):
+
+    def test_good_case(self):
+        root = BinaryTree(3)
+        root.left = BinaryTree(9)
+        root.right = BinaryTree(20)
+        root.right.left = BinaryTree(15)
+        root.right.right = BinaryTree(7)
+
+        self.assertEqual(branchSums(root), 24)
+
+    def test_bad_case(self):
+        root = None
+
+        self.assertEqual(branchSums(root), 0)
+
+    def test_empty(self):
+        root = BinaryTree(0)  # Creating a tree with only the root node
+
+        self.assertEqual(branchSums(root), 0)  # Expected sum of values is 0
+
+
+if __name__ == '__main__':
+    unittest.main()
