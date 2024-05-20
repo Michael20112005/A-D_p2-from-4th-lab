ce06270e7ffcd326b4aca8db579681a7e1d871d3
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-20 17:04:54 +0300

    tests for 1 lab

--- test/test.py
+++ test/test.py
@@ -1,8 +1,36 @@
-N = 0
-pairs = []
+from plant_pumpkins import plant_pumpkins_route
+import unittest
 
-N = 3
-pairs = [(1, 2), (2, 4), (3, 5)]
 
-N = 1
-pairs = [(1, 6)]
+class TestPlantPumpkinsRoute(unittest.TestCase):
+
+    def test_matrix_3x3(self):
+        matrix = [
+            [1, 2, 3],
+            [8, 7, 6],
+            [9, 10, 11]
+        ]
+        expected_result = [1, 2, 3, 6, 7, 8, 9, 10, 11]
+        self.assertEqual(plant_pumpkins_route(matrix), expected_result)
+
+    def test_matrix_2x4(self):
+        matrix = [
+            [1, 2, 3, 4],
+            [8, 7, 6, 5]
+        ]
+        expected_result = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.assertEqual(plant_pumpkins_route(matrix), expected_result)
+
+    def test_matrix_4x2(self):
+        matrix = [
+            [1, 2],
+            [6, 5],
+            [7, 8],
+            [12, 11]
+        ]
+        expected_result = [1, 2, 5, 6, 7, 8, 11, 12]
+        self.assertEqual(plant_pumpkins_route(matrix), expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
