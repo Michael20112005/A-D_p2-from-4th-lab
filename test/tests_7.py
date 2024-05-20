08b00ec288b98ffdb9d361949eb9eca2fbd42094
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-20 20:27:26 +0300

    test_7Lab

---
+++ test/tests_7.py
@@ -0,0 +1,25 @@
+from Rabin_Karp import rabin_karp_search
+import unittest
+
+
+class TestRabinKarpSearch(unittest.TestCase):
+
+    def test_good_case(self):
+        haystack = "ABABDABACDABABCABAB"
+        needle = "AB"
+        expected = [0, 2, 5, 10, 12, 15, 17]
+        self.assertEqual(rabin_karp_search(haystack, needle), expected)
+
+    def test_bad_case(self):
+        haystack = "ABCDEF"
+        needle = "XYZ"
+        self.assertEqual(rabin_karp_search(haystack, needle), [])
+
+    def test_empty(self):
+        haystack = "ABCDEF"
+        needle = ""
+        self.assertEqual(rabin_karp_search(haystack, needle), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
