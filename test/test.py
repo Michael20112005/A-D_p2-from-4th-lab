4b3ed9b25d004be7ad962d7b75c07874fef70fe1
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-20 19:39:43 +0300

    test_6

--- test/test.py
+++ test/test.py
@@ -1,36 +1,28 @@
-from plant_pumpkins import plant_pumpkins_route
+from find_union import weddings_and_tribes
 import unittest
 
 
-class TestPlantPumpkinsRoute(unittest.TestCase):
+class TestWeddingsAndTribes(unittest.TestCase):
 
-    def test_matrix_3x3(self):
-        matrix = [
-            [1, 2, 3],
-            [8, 7, 6],
-            [9, 10, 11]
-        ]
-        expected_result = [1, 2, 3, 6, 7, 8, 9, 10, 11]
-        self.assertEqual(plant_pumpkins_route(matrix), expected_result)
+    def test_good_case(self):
+        pairs_list = [(1, 2), (2, 3), (3, 4), (5, 6)]
+        total_pairs, possible_pairs = weddings_and_tribes(pairs_list)
+        self.assertEqual(total_pairs, 4)
+        self.assertSetEqual(possible_pairs, {(1, 6), (5, 2), (5, 4), (3, 6)})
 
-    def test_matrix_2x4(self):
-        matrix = [
-            [1, 2, 3, 4],
-            [8, 7, 6, 5]
-        ]
-        expected_result = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.assertEqual(plant_pumpkins_route(matrix), expected_result)
+    def test_bad_case(self):
+        pairs_list = [(1, 2), (2, 3), (3, 4), (5, 6), (1, 5)]
+        total_pairs, possible_pairs = weddings_and_tribes(pairs_list)
+        self.assertEqual(total_pairs, 0)
+        self.assertSetEqual(possible_pairs, set())
 
-    def test_matrix_4x2(self):
-        matrix = [
-            [1, 2],
-            [6, 5],
-            [7, 8],
-            [12, 11]
-        ]
-        expected_result = [1, 2, 5, 6, 7, 8, 11, 12]
-        self.assertEqual(plant_pumpkins_route(matrix), expected_result)
+    def test_another_good_case(self):
+        pairs_list = [(1, 2), (2, 3), (1, 3), (4, 5)]
+        total_pairs, possible_pairs = weddings_and_tribes(pairs_list)
+        self.assertEqual(total_pairs, 3)
+        self.assertSetEqual(possible_pairs, {(3, 4), (1, 4), (5, 2)})
 
 
 if __name__ == '__main__':
     unittest.main()
+    
