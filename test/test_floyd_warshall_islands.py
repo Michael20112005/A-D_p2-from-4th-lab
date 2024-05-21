77fc1a88f603a93ea70ac7d2e5d344938cdb5f77
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-21 11:56:31 +0300

    Update test_floyd_warshall_islands.py

--- test/test_floyd_warshall_islands.py
+++ test/test_floyd_warshall_islands.py
@@ -3,37 +3,40 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 import numpy as np
-import csv
-
-from floyd_warshall_islands import main
-
+from floyd_warshall_islands import calculate_total_cable_length
 
 class TestSubmarineCableLength(unittest.TestCase):
 
-    def read_adj_matrix_from_file(self, filename):
-        with open(filename, 'r') as file:
-            reader = csv.reader(file)
-            adj_matrix = np.array([[int(val) for val in row] for row in reader])
-        return adj_matrix
-
     def setUp(self):
-        self.total_cable_length = main()
+        self.INF = float('inf')
 
     def test_valid_distances(self):
-        adj_matrix = self.read_adj_matrix_from_file('../src/Islands.csv')
-        self.assertTrue(self.total_cable_length >= 0)
+        adj_matrix = np.array([
+            [0, 1, self.INF],
+            [1, 0, 1],
+            [self.INF, 1, 0]
+        ])
+        total_cable_length = calculate_total_cable_length(adj_matrix)
+        self.assertTrue(total_cable_length >= 0)
 
     def test_cable_length_within_range(self):
-        adj_matrix = self.read_adj_matrix_from_file('../src/Islands.csv')
-        self.assertTrue(0 <= self.total_cable_length < 999999)
+        adj_matrix = np.array([
+            [0, 2, self.INF, 1],
+            [2, 0, 3, self.INF],
+            [self.INF, 3, 0, 1],
+            [1, self.INF, 1, 0]
+        ])
+        total_cable_length = calculate_total_cable_length(adj_matrix)
+        self.assertTrue(0 <= total_cable_length < 999999)
 
     def test_large_distances(self):
-        adj_matrix = self.read_adj_matrix_from_file('../src/Islands.csv')
-        self.assertTrue(self.total_cable_length < 999999)
-
-
-        #too general tests. Do not cover extreme cases
-
+        adj_matrix = np.array([
+            [0, 1, self.INF],
+            [1, 0, 1000000],
+            [self.INF, 1000000, 0]
+        ])
+        total_cable_length = calculate_total_cable_length(adj_matrix)
+        self.assertTrue(total_cable_length < 999999)
 
 if __name__ == '__main__':
     unittest.main()
