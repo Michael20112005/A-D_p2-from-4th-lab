3230fd93c6e6b0f3016982e0146cc35f3476f45e
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-21 11:43:27 +0300

    Update test_graph_cycle.py

--- test/test_graph_cycle.py
+++ test/test_graph_cycle.py
@@ -1,5 +1,8 @@
-from graph_cycle import bfs
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from graph_cycle import bfs
 
 
 class TestBFS(unittest.TestCase):
