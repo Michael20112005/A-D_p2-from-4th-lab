71f1d9a8b025f8bbd4f3379211e2a46770a07f0c
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-21 11:44:55 +0300

    Update test_rabin_karp.py

--- test/test_rabin_karp.py
+++ test/test_rabin_karp.py
@@ -1,5 +1,8 @@
-from rabin_karp import rabin_karp_search
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from rabin_karp import rabin_karp_search
 
 
 class TestRabinKarpSearch(unittest.TestCase):
