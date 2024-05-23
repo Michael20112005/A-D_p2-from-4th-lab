577fa8ec3e82aee5f14692cf843ea36e3dd220b9
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-23 08:19:54 +0300

    Update test_floyd_warshall_islands.py

--- test/test_floyd_warshall_islands.py
+++ test/test_floyd_warshall_islands.py
@@ -2,40 +2,45 @@ import unittest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+import numpy as np
 from floyd_warshall_islands import calculate_total_cable_length
 
-class TestSubmarineCableLength(unittest.TestCase):
 
-    def setUp(self):
-        self.INF = float('inf')
+class TestCableLength(unittest.TestCase):
 
-    def test_valid_distances(self):
+    def test_small_graph(self):
         adj_matrix = np.array([
-            [0, 1, self.INF],
+            [0, 1, np.inf],
             [1, 0, 1],
-            [self.INF, 1, 0]
+            [np.inf, 1, 0]
         ])
-        total_cable_length = calculate_total_cable_length(adj_matrix)
-        self.assertAlmostEqual(total_cable_length, 5)  # Expected total length
+        result = calculate_total_cable_length(adj_matrix)
+        expected = 8.0
+        self.assertAlmostEqual(result, expected, places=2)
 
-    def test_cable_length_within_range(self):
+    def test_medium_graph(self):
         adj_matrix = np.array([
-            [0, 2, self.INF, 1],
-            [2, 0, 3, self.INF],
-            [self.INF, 3, 0, 1],
-            [1, self.INF, 1, 0]
+            [0, 2, np.inf, 6],
+            [2, 0, 3, 8],
+            [np.inf, 3, 0, np.inf],
+            [6, 8, np.inf, 0]
         ])
-        total_cable_length = calculate_total_cable_length(adj_matrix)
-        self.assertAlmostEqual(total_cable_length, 11)  # Expected total length
+        result = calculate_total_cable_length(adj_matrix)
+        expected = 70.0
+        self.assertAlmostEqual(result, expected, places=2)
 
-    def test_large_distances(self):
+    def test_large_graph(self):
         adj_matrix = np.array([
-            [0, 1, self.INF],
-            [1, 0, 1000000],
-            [self.INF, 1000000, 0]
+            [0, 2, np.inf, 6, np.inf],
+            [2, 0, 3, 8, 5],
+            [np.inf, 3, 0, np.inf, 7],
+            [6, 8, np.inf, 0, 9],
+            [np.inf, 5, 7, 9, 0]
         ])
-        total_cable_length = calculate_total_cable_length(adj_matrix)
-        self.assertAlmostEqual(total_cable_length, 1000002)  # Expected total length
+        result = calculate_total_cable_length(adj_matrix)
+        expected = 126.0
+        self.assertAlmostEqual(result, expected, places=2)
+
 
 if __name__ == '__main__':
     unittest.main()
