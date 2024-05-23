4289264686889a0196043a9d82a08f83d9f02080
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2024-05-23 08:05:06 +0300

    Update test_calculate_max_length.py
    made good tests to the updated code

--- test/test_calculate_max_length.py
+++ test/test_calculate_max_length.py
@@ -1,9 +1,9 @@
-import math
-from calculate_max_length import calculate_max_length
 import unittest
 import sys
 import os
+import math
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from calculate_max_length import max_length
 
 
 class TestCalculateMaxLength(unittest.TestCase):
@@ -12,25 +12,25 @@ class TestCalculateMaxLength(unittest.TestCase):
         w = 4
         heights = [56, 18, 17, 94, 23, 7, 21, 94, 29, 54, 44, 26, 86, 79, 4, 15, 5, 91, 25, 17, 88, 66, 28, 2, 95, 97,
                    60, 93, 40, 70, 75, 48, 38, 51, 34, 52, 87, 8, 62, 77, 35, 52, 3, 93, 34, 57, 51, 11, 39, 72]
-        result = calculate_max_length(w, heights)
+        result = max_length(w, heights)
         self.assertEqual(result, 2738.18)
 
     def test_single_pole(self):
         w = 5
         heights = [50]
-        result = calculate_max_length(w, heights)
+        result = max_length(w, heights)
         self.assertEqual(result, 0.0)
 
     def test_max_heights(self):
         w = 1
         heights = [100, 100, 100, 100, 100]
-        result = calculate_max_length(w, heights)
+        result = max_length(w, heights)
         self.assertAlmostEqual(result, 4 * math.sqrt(1 + 99 ** 2), places=2)
 
     def test_large_w(self):
         w = 100
         heights = [1, 1, 1, 1, 1]
-        result = calculate_max_length(w, heights)
+        result = max_length(w, heights)
         self.assertAlmostEqual(result, 4 * math.sqrt(100 ** 2 + 0 ** 2), places=2)
 
 
